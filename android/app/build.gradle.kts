f2dfd6bb0e3a929d41b18567b1a0a88ce992c5c1
arthurphilippe1729-ai <arthur.philippe1729@gmail.com>
2025-08-14 10:26:21 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,18 +1,28 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    // ...
+    namespace = "com.example.app_todo_plus"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.app_todo_plus"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
